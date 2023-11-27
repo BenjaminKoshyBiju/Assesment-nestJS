32ea4147d9e1720ffffb74a3a3c2aa9dfeb2115c
BenjaminKoshyBiju <bennn455@gmail.com>
2023-11-27 18:26:13 +0530

    created tests for controller.specs.ts and services.spec.ts

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +17,197 @@ describe('UserController', () => {
       providers: [UserService],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'benjamin',
+        username: 'ben',
+        email: 'benjamin@gmail.com"',
+        age: 25,
+        password: 'Password',
+        gender: 'm'
+      };
+
+      // Mock the UserService's createUser method
+      jest.spyOn(userService, 'createUser').mockResolvedValue({ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' } as User);
+
+      const result = await userController.create(createUserDto);
+
+      expect(result).toEqual({ /* mock created user data */ });
+    });
+
+    it('should handle conflict when creating a user with an existing username', async () => {
+      const createUserDto: CreateUserDto = {
+      
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'
+      };
+
+     
+      jest.spyOn(userService, 'createUser').mockRejectedValue(new ConflictException('Username already exists'));
+
+      
+      await expect(userController.create(createUserDto)).rejects.toThrow(ConflictException);
+    });
+  });
+
+  describe('validateUser', () => {
+    it('should validate user credentials', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'
+      };
+
+      // Mock the UserService's validateUser method
+      jest.spyOn(userService, 'validateUser').mockResolvedValue({ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' } as User);
+
+      const result = await userController.validateUser(createUserDto);
+
+      expect(result).toEqual({ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' });
+    });
+
+    it('should handle unauthorized when validating with incorrect credentials', async () => {
+      const createUserDto: CreateUserDto = {
+        
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'
+      };
+
+      // Mock the UserService's validateUser method to throw an UnauthorizedException
+      jest.spyOn(userService, 'validateUser').mockRejectedValue(new UnauthorizedException('Invalid credentials'));
+
+      // Assert that calling validateUser with incorrect credentials throws an UnauthorizedException
+      await expect(userController.validateUser(createUserDto)).rejects.toThrowError(UnauthorizedException);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      // Mock the UserService's findAllUsers method
+      jest.spyOn(userService, 'findAllUsers').mockResolvedValue([{ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' }] as User[]);
+
+      const result = await userController.findAll();
+
+      expect(result).toEqual([{ 
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' }]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single user', async () => {
+      const userId = '1';
+
+      // Mock the UserService's viewUser method
+      jest.spyOn(userService, 'viewUser').mockResolvedValue({ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' } as User);
+
+      const result = await userController.findOne(userId);
+
+      expect(result).toEqual({ 
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'});
+    });
+
+    it('should handle not found when retrieving a non-existing user', async () => {
+      const userId = '999';
+
+      // Mock the UserService's viewUser method to throw a NotFoundException
+      jest.spyOn(userService, 'viewUser').mockRejectedValue(new NotFoundException('User not found'));
+
+      // Assert that calling findOne with a non-existing user ID throws a NotFoundException
+      await expect(userController.findOne(userId)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user', async () => {
+      const userId = '1';
+      const updateUserDto: UpdateUserDto = {
+        
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'
+      };
+
+      // Mock the UserService's updateUser method
+      jest.spyOn(userService, 'updateUser').mockResolvedValue({ id: 2,
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm' } as User);
+
+      const result = await userController.update(userId, updateUserDto);
+
+      expect(result).toEqual({ 
+        name: 'Benjamin',
+        username: 'Ben14',
+        email: 'benjamin@gmail.com',
+        age: 13,
+        password: 'Benjamin',
+        gender: 'm'});
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+
 
 describe('UserService', () => {
   let service: UserService;
@@ -15,4 +19,144 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('it should return an array of users', async ()=>{
+    const mockUsers: User[]=[
+    {
+      id: 2,
+      name: 'Benjamin',
+      username: 'Ben14',
+      email: 'benjamin@gmail.com',
+      age: 13,
+      password: 'Benjamin',
+      gender: 'm',
+    },
+  ];
+    jest.spyOn(service['userRepository'], 'find').mockResolvedValue(mockUsers)
+
+    const result = await service.findAllUsers()
+    expect(result).toEqual(mockUsers);
+  })
+
+
 });
+
+
+describe('createUser', () => {
+  let service: UserService;
+  it('should create a user', async () => {
+    const createUserDto: CreateUserDto = {
+      name: 'Benjamin',
+      age: 25,
+      email: 'ben@example.com',
+      username: 'ben10',
+      password: 'password123',
+      gender: 'm',
+    };
+
+    
+    jest.spyOn(service['userRepository'], 'findOneBy').mockResolvedValue(null);
+
+  
+    const createdUser: User = {
+      "id": 4,
+      "name": "Kobe",
+      "username": "kob14",
+      "email": "kobe@gmail.com",
+      "age": 43,
+      "password": "Basketball",
+      "gender": "m"
+  };
+    jest.spyOn(service['userRepository'], 'save').mockResolvedValue(createdUser);
+
+    const result = await service.createUser(createUserDto, createUserDto.username);
+
+    expect(result).toEqual(createdUser);
+  });
+
+  it('should throw ConflictException if username already exists', async () => {
+    const createUserDto: CreateUserDto = { "name": "Benjamin",
+    "username": "Ben14",
+    "email": "benjamin@gmail.com",
+    "age": 13,
+    "password": "Benjamin",
+    "gender": "m"
+      // ... mock user data
+    };
+
+    // Mock the repository's findOneBy method to simulate an existing user
+    jest.spyOn(service['userRepository'], 'findOneBy').mockResolvedValue({ id: 1,
+      name: 'benjamin',
+      username: 'ben',
+      email: 'benjamin@gmail.com"',
+      age: 25,
+      password: 'Password',
+      gender: 'm',
+
+    } as User);
+
+    // Assert that calling createUser with an existing username throws ConflictException
+    await expect(service.createUser(createUserDto, createUserDto.username)).rejects.toThrowError('Username already exists');
+  });
+
+  describe('validateUser', () => {
+    let userService: UserService;
+    it('should validate user credentials', async () => {
+      const createUserDto: CreateUserDto = { "name": "Benjamin",
+      "username": "Ben14",
+      "email": "benjamin@gmail.com",
+      "age": 13,
+      "password": "Benjamin",
+      "gender": "m"
+        // ... mock user data
+      }
+
+      const loginUsername = 'john.doe';
+      const loginPassword = 'password123';
+
+      // Mock the repository's findOneBy method to simulate an existing user
+      const existingUser: User = {id: 1,
+        name: 'benjamin',
+        username: 'ben',
+        email: 'benjamin@gmail.com"',
+        age: 25,
+        password: 'Password',
+        gender: 'm' };
+      jest.spyOn(userService['userRepository'], 'findOneBy').mockResolvedValue(existingUser);
+
+      // Mock bcrypt.compare to always return true (matching password)
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true);
+
+      const result = await userService.validateUser(createUserDto, loginUsername, loginPassword);
+
+      expect(result).toEqual(existingUser);
+    });
+
+    it('should throw UnauthorizedException if user not found or password does not match', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'benjamin',
+        username: 'ben',
+        email: 'benjamin@gmail.com"',
+        age: 25,
+        password: 'Password', 
+        gender: 'm'
+      };
+
+      const loginUsername = 'benjamin';
+      const loginPassword = 'password123';
+
+      
+      jest.spyOn(userService['userRepository'], 'findOneBy').mockResolvedValue(null);
+
+      // Mock bcrypt.compare to always return false (non-matching password)
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(false);
+
+      
+      await expect(userService.validateUser(createUserDto, loginUsername, loginPassword)).rejects.toThrow('Invalid credentials');
+    });
+  });
+});
+
+
+
+
